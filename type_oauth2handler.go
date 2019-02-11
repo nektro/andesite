b1177ff574a07db9f9780c501611914d83266af2
Meghan <nektromc@gmail.com>
2019-02-10 20:59:02 -0500

    move scope property down to make structs look better

--- type_oauth2handler.go
+++ type_oauth2handler.go
@@ -2,9 +2,9 @@ package main
 
 type Oauth2Provider struct {
 	authorizeURL string
-	scope        string
 	tokenURL     string
 	meURL        string
+	scope        string
 	dbPrefix     string
 	nameProp     string
 	namePrefix   string
@@ -14,27 +14,27 @@ var (
 	Oauth2Providers = map[string]Oauth2Provider{
 		"discord": Oauth2Provider{
 			"https://discordapp.com/api/oauth2/authorize",
-			"identify",
 			"https://discordapp.com/api/oauth2/token",
 			"https://discordapp.com/api/users/@me",
+			"identify",
 			"",
 			"username",
 			"@",
 		},
 		"reddit": Oauth2Provider{
 			"https://old.reddit.com/api/v1/authorize",
-			"identity",
 			"https://old.reddit.com/api/v1/access_token",
 			"https://oauth.reddit.com/api/v1/me",
+			"identity",
 			"1:",
 			"name",
 			"u/",
 		},
 		"github": Oauth2Provider{
 			"https://github.com/login/oauth/authorize",
-			"read:user",
 			"https://github.com/login/oauth/access_token",
 			"https://api.github.com/user",
+			"read:user",
 			"2:",
 			"login",
 			"@",
