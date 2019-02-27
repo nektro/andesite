48fded66a4566b89e6f0630fdce7cca401d390c5
Meghan <nektromc@gmail.com>
2019-02-27 03:18:46 -0500

    add fasthttp support to FusingFileSystem

--- multi_fs.go
+++ multi_fs.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"io/ioutil"
+	"mime"
 	"net/http"
+	"path"
+	"strings"
+
+	"github.com/valyala/fasthttp"
 )
 
 type FusingFileSystem struct {
@@ -18,4 +24,21 @@ func (ffs FusingFileSystem) Open(name string) (http.File, error) {
 		errr = err
 	}
 	return nil, errr
+}
+
+func (ffs FusingFileSystem) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
+	fpath := string(ctx.Request.RequestURI())
+	if strings.HasSuffix(fpath, "/") {
+		fpath += "index.html"
+	}
+	// fmt.Println("GET: ", fpath)
+	file, err := ffs.Open(fpath)
+	if err == nil {
+		bytes, _ := ioutil.ReadAll(file)
+		ctx.SetStatusCode(fasthttp.StatusOK)
+		ctx.SetContentType(mime.TypeByExtension(path.Ext(fpath)))
+		ctx.SetBody(bytes)
+	} else {
+		ctx.SetStatusCode(fasthttp.StatusNotFound)
+	}
 }
