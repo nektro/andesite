e1d6e1047ebab507aea6ed97ce17937e9966ca74
Meghan Denny <nektromc@gmail.com>
2019-07-15 22:37:51 -0400

    remove makefile and change to bash script

---
+++ scripts/build_all.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+function build_template() {
+    CGO_ENABLED=0
+    GOOS=$1
+    GOARCH=$2
+    TAG=$(date +'%Y.%m.%d')-$(git log --format=%h -1)
+    echo $TRAVIS_TAG
+    go build -ldflags="-s -w" -o ./dist/andesite-v$TAG-$GOOS-$GOARCH
+}
+
+build_template linux amd64
