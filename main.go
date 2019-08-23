32082253427738658e78fc8ede184b0e1913cc83
Meghan Denny <nektromc@gmail.com>
2019-08-23 01:08:45 -0400

    add url_name helper to encode special characters in names for urls

--- main.go
+++ main.go
@@ -215,6 +215,13 @@ func main() {
 	}
 
 	//
+	// handlebars helpers
+
+	raymond.RegisterHelper("url_name", func(x string) string {
+		return strings.ReplaceAll(url.PathEscape(x), "%2F", "/")
+	})
+
+	//
 	// http server setup and launch
 
 	statikFS, err := fs.New()
