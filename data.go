ee7bee887a77e10e9fb17b4a3b53680f1b00f9d9
Meghan Denny <nektromc@gmail.com>
2019-09-10 02:59:43 -0400

    data - remove unused vars randomKey and store, util.GetSession is now used

--- data.go
+++ data.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"github.com/gorilla/securecookie"
-	"github.com/gorilla/sessions"
 	"github.com/mitchellh/go-homedir"
 	oauth2 "github.com/nektro/go.oauth2"
 
@@ -18,7 +16,5 @@ var (
 	config          *itypes.Config
 	oauth2AppConfig *oauth2.AppConf
 	oauth2Provider  itypes.Oauth2Provider
-	randomKey       = securecookie.GenerateRandomKey(32)
-	store           = sessions.NewCookieStore(randomKey)
 	homedirPath, _  = homedir.Dir()
 )
