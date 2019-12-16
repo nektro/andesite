ba7c673e84538bc68e9af55a63312cec8ab31ea4
Cat, The Hacker <31106839+CatTheHacker@users.noreply.github.com>
2019-12-16 09:54:41 +0000

    Add CD using GitHub Actions

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -5,27 +5,49 @@ init() {
     $GOPATH/bin/statik -src="./www/"
 }
 build_template() {
-    export CGO_ENABLED=1
     export GOOS=$1
     export GOARCH=$2
-    export GOARM=7
+    if [ $GOOS = 'windows' ]
+    then
+        export CGO_ENABLED=1
+        if [ $GOARCH = 'amd64' ]
+        then
+            export CC="x86_64-w64-mingw32-gcc"
+            export CXX="x86_64-w64-mingw32-g++"
+        else
+            export CC="i686-w64-mingw32-gcc"
+            export CXX="i686-w64-mingw32-g++"
+        fi
+    fi
     EXT=$3
-    TAG=$(date +'%Y.%m.%d')-$(git log --format=%h -1)
-    echo $TAG-$GOOS-$GOARCH
-    go build -ldflags="-s -w" -o ./bin/andesite-v$TAG-$GOOS-$GOARCH$EXT
+    TAG=$(date +'%Y.%m.%d').$(git log --format=%h -1)
+    if [ $GOARCH = 'arm' ]
+    then
+        export GOARM=6
+        echo $TAG-$GOOS-armv$GOARM
+        go build -ldflags="-s -w" -o ./bin/andesite-v$TAG-$GOOS-armv$GOARM
+    elif [ $GOARCH = 'arm64' ]
+    then
+        export GOARM=7
+        echo $TAG-$GOOS-arm64v$GOARM
+        go build -ldflags="-s -w" -o ./bin/andesite-v$TAG-$GOOS-arm64v$GOARM
+    else
+        echo $TAG-$GOOS-$GOARCH
+        go build -ldflags="-s -w" -o ./bin/andesite-v$TAG-$GOOS-$GOARCH$EXT
+    fi
 }
 
 init
-# build_template darwin 386
-# build_template darwin amd64
-# build_template dragonfly amd64
-# build_template freebsd 386
-# build_template freebsd amd64
-# build_template freebsd arm
-# build_template linux 386
+build_template darwin 386
+build_template darwin amd64
+build_template dragonfly amd64
+build_template freebsd 386
+build_template freebsd amd64
+build_template freebsd arm
+build_template linux 386
 build_template linux amd64
-# build_template linux arm
-# build_template linux arm64
+build_template linux arm
+build_template linux arm64
 # build_template linux ppc64
 # build_template linux ppc64le
 # build_template linux mips
@@ -39,12 +61,12 @@ build_template linux amd64
 # build_template netbsd 386
 # build_template netbsd amd64
 # build_template netbsd arm
-# build_template openbsd 386
-# build_template openbsd amd64
-# build_template openbsd arm
+build_template openbsd 386
+build_template openbsd amd64
+build_template openbsd arm
 # build_template plan9 386
 # build_template plan9 amd64
 # build_template plan9 arm
-# build_template solaris amd64
-# build_template windows 386
-# build_template windows amd64
+build_template solaris amd64
+build_template windows 386 .exe
+build_template windows amd64 .exe
