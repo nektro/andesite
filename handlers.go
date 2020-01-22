45308ddddde3b56ab4f3b4487655ac343e9b49e9
Meghan Denny <nektromc@gmail.com>
2020-01-22 16:24:52 -0500

    www/admin - only show discord role share table if guild and bot token are passed

--- handlers.go
+++ handlers.go
@@ -250,14 +250,16 @@ func handleAdmin(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
+	dc := idata.Config.GetDiscordClient()
 	etc.WriteHandlebarsFile(r, w, "/admin.hbs", map[string]interface{}{
-		"user": user.Snowflake,
-		"base": idata.Config.HTTPBase,
-		"name": oauth2.ProviderIDMap[user.Provider].NamePrefix + user.Name,
-		"auth": oauth2.ProviderIDMap[user.Provider].ID,
-		"accesses":       iutil.QueryAllAccess(),
-		"shares":         iutil.QueryAllShares(),
-		"discord_shares": iutil.QueryAllDiscordRoleAccess(),
+		"user":                  user.Snowflake,
+		"base":                  idata.Config.HTTPBase,
+		"name":                  oauth2.ProviderIDMap[user.Provider].NamePrefix + user.Name,
+		"auth":                  oauth2.ProviderIDMap[user.Provider].ID,
+		"discord_role_share_on": len(dc.Extra1) > 0 && len(dc.Extra2) > 0,
+		"accesses":              iutil.QueryAllAccess(),
+		"shares":                iutil.QueryAllShares(),
+		"discord_shares":        iutil.QueryAllDiscordRoleAccess(),
 	})
 }
 
