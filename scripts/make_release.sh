ab03a6302e8a8669e5de63dc4221f56d7204525d
Meghan Denny <nektromc@gmail.com>
2020-01-23 18:46:38 -0500

    add scripts/make_release

---
+++ scripts/make_release.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+init() {
+    date=$(date +'%Y.%m.%d')
+    version=${CIRCLE_BUILD_NUM-$date}
+    tag=v$version-$(git log --format=%h -1)
+    go get -v -u github.com/tcnksm/ghr
+    $GOPATH/bin/ghr \
+        -t ${GITHUB_TOKEN} \
+        -u ${CIRCLE_PROJECT_USERNAME} \
+        -r ${CIRCLE_PROJECT_REPONAME} \
+        "$tag" \
+        "/artifacts/"
+}
+
+init
