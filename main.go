606f5392131e556a9db0e5ecd2680ab60b623e56
Meghan Denny <nektromc@gmail.com>
2020-02-10 13:29:32 -0500

    move helperOA2SaveInfo to main

--- main.go
+++ main.go
@@ -168,3 +168,16 @@ func main() {
 
 	etc.StartServer(idata.Config.Port)
 }
+
+func helperOA2SaveInfo(w http.ResponseWriter, r *http.Request, provider string, id string, name string, resp map[string]interface{}) {
+	sess := etc.GetSession(r)
+	sess.Values["provider"] = provider
+	sess.Values["user"] = id
+	sess.Values["name"] = name
+	sess.Values[provider+"_access_token"] = resp["access_token"]
+	sess.Values[provider+"_expires_in"] = resp["expires_in"]
+	sess.Values[provider+"_refresh_token"] = resp["refresh_token"]
+	sess.Save(r, w)
+	iutil.QueryAssertUserName(provider, id, name)
+	Log("[user-login]", provider, id, name)
+}
