fae064442b3d671314aec34ee3e23d38d8e62c25
Meghan <hello@nektro.net>
2020-09-07 00:53:48 -0700

    scripts/build_all- remove unused targets

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -18,35 +18,4 @@ build_template() {
 }
 
 init
-# build_template darwin 386
-# build_template darwin amd64
-# build_template dragonfly amd64
-# build_template freebsd 386
-# build_template freebsd amd64
-# build_template freebsd arm
-# build_template linux 386
 build_template linux amd64
-# build_template linux arm
-# build_template linux arm64
-# build_template linux ppc64
-# build_template linux ppc64le
-# build_template linux mips
-# build_template linux mipsle
-# build_template linux mips64
-# build_template linux mips64le
-# build_template linux s390x
-# build_template nacl 386
-# build_template nacl amd64p32
-# build_template nacl arm
-# build_template netbsd 386
-# build_template netbsd amd64
-# build_template netbsd arm
-# build_template openbsd 386
-# build_template openbsd amd64
-# build_template openbsd arm
-# build_template plan9 386
-# build_template plan9 amd64
-# build_template plan9 arm
-# build_template solaris amd64
-# build_template windows 386 .exe
-# build_template windows amd64 .exe
