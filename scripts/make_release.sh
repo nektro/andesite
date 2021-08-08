fcc4f0871b09631b3b9ef72758f503fc8481b141
Meghan Denny <hello@nektro.net>
2021-08-07 22:36:06 -0700

    scripts- make_release- remove githash from tag name

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
-init() {
-    date=$(date +'%Y.%m.%d')
-    version=${CIRCLE_BUILD_NUM-$date}
-    tag=v$version-$(git log --format=%h -1)
-    go get -v -u github.com/tcnksm/ghr
-    $GOPATH/bin/ghr \
-        -t ${GITHUB_TOKEN} \
-        -u ${CIRCLE_PROJECT_USERNAME} \
-        -r ${CIRCLE_PROJECT_REPONAME} \
-        -b "$(./scripts/changelog.sh)" \
-        "$tag" \
-        "./bin/"
-}
-
-init
+date=$(date +'%Y.%m.%d')
+version=${CIRCLE_BUILD_NUM-$date}
+tag=v$version
+go get -v -u github.com/tcnksm/ghr
+$GOPATH/bin/ghr \
+    -t ${GITHUB_TOKEN} \
+    -u ${CIRCLE_PROJECT_USERNAME} \
+    -r ${CIRCLE_PROJECT_REPONAME} \
+    -b "$(./scripts/changelog.sh)" \
+    "$tag" \
+    "./bin/"
